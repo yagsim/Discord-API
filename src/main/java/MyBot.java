5c5b46e7a274b646ac24d66a4fec06272ab2d773
yagsim <yagosimofernandez@gmail.com>
2022-03-16 14:00:46 +0100

    explicación boot

--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -7,7 +7,10 @@ public class MyBot {
     public static void main(String[] args) {
         //Crearemos un bot(programa en computadora que nos dejara interactuar con Discord)
         //para interactuar con Discord nos ofrece DiscordClient y el punto de acceso se hara con .create
+        //El boot se generara abriendo sesión y entrando en new applicattion donde nos pedira un nombre con el que poder acceder al token ,agregareremos el boot y resetearemos el token ,asi nos dara un código que sera el introducido a continuación
         DiscordClient client = DiscordClient.create("OTUzNjM0MjMwODI5NzI3ODM2.YjHbFg.2SOCzGIau6MoIqYi_BXQLnskGF4");   //El boot restringe
+
+
         Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
 
         login.block();
